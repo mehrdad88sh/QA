2ef9301088859806a26234002a6f11b38c4d6879
Mehrdad88sh <mehrdad88sh@gmial.com>
2021-12-22 01:09:04 +0330

    fix:change test enviroment

--- protools/api/Variables/Variables.py
+++ protools/api/Variables/Variables.py
@@ -6,7 +6,5 @@ class Variables(object):
     def get_variables(self):
         return {
         'staging': 'https://staging.mielse.com',
-        'stagingMock': 'https://staging.mielse.com/mock/getCode?mobile',
-        'pr': 'https://pr10303.mielse.com',
-        'prMock': 'https://pr10303.mielse.com/mock/getCode?mobile'
+        'stagingMock': 'https://staging.mielse.com/mock/getCode?mobile'
 }
