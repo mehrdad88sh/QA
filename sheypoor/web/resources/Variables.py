c126c87fccd5029c26341b9b3545fbb87473b25b
Mehrdad88sh <mehrdad88sh@gmial.com>
2022-06-20 16:18:55 +0430

    add variable file

---
+++ sheypoor/web/resources/Variables.py
@@ -0,0 +1,79 @@
+# ========================================Login Elements===============================================
+My_account_button="css:.my-account"
+My_Account_Icon_Loading="css:.my-account.loading"
+My_listing_button="css:a[href$='myListings']"
+Login_Button="css:[data-test-id='login-submit-tel']"
+Confirm_Button="css:[data-test-id='verfication-submit']"
+Submit_Listing_Button="css:[data-test-id='listing-submit']"
+Mobile_Number_TextField="name:username"
+Verfication_Code_Textfield="name:verification_code"
+# =====================================================================================================
+
+# ========================================Category Elements===============================================
+Popup_Title="css:[data-test-id='popup-title']"
+Popup_Category_Selection="[data-test-id='popup-title']"
+Categories_Loding_Icon="css:[data-test-id='categories-loading']"
+Listing_Select_Category="css:[data-test-id='listing-select-category']"
+Categories_list="css:[data-test-id='mode-category']"
+SubCategories_list="css:[data-test-id='mode-subCategory']"
+Main_Categry="css:[data-test-id='t-category-{}']"
+Sub_Category="css:[data-test-id='t-subCategory-{}']"
+# =====================================================================================================
+
+# ========================================Text Messeges===============================================
+Home_Page_Message="نیازمندیهای رایگان شیپور"
+Login_Register_Button="ورود / ثبت نام"
+Login_Message="لطفا برای ورود یا ثبت نام شماره تلفن همراه خود را وارد کنید."
+Phone_Number_Verification_Message="تائید شماره موبایل"
+My_Listing_Message="آگهی‌‌های من"
+Login_button="//a[contains(text(),' ورود / ثبت نام ')]"
+Free_Listing_Submit_Button="ثبت آگهی رایگان"
+Form_Clear_Button="پاکسازی فرم"
+Successful_Post_Listing_Message="آگهی شما با موفقیت به صورت خودکار فعال شد."
+Property_Type_Title="نوع ملک"
+Apartmant="آپارتمان"
+# ====================================================================================================
+
+
+# ======================================Location Elements======================================================
+Location_Component="css:[data-test-id='listing-formComponentLocation-location']"
+Location_Label="name:location.label"
+Provinces_List_Popup="css:[data-test-id='locationSearch-popup-provinceList'] li:nth-child({})"
+Provinces_List="css:[data-test-id='mode-province']"
+Select_Province="css:[data-test-id='t-province-{}']"
+Province_Count="css:[data-test-id='locationSearch-popup-provinceList'] li"
+Cities_Count="css:[data-test-id='locationSearch-popup-provinceList'] li"
+Cities_List_Popup="css:[data-test-id='locationSearch-popup-provinceList'] li:nth-child({})"
+Cities_List="css:[data-test-id='mode-city']"
+Select_City="css:[data-test-id='t-city-{}']"
+Districts_Count="css:[data-test-id='locationSearch-popup-provinceList'] li"
+Districts_List_Popup="css:[data-test-id='locationSearch-popup-provinceList'] li:nth-child({})"
+Districts_List="css:[data-test-id='mode-district']"
+Select_District="css:[data-test-id='t-district-{}']"
+Neighborhood_TextField="name:neighbourhood"
+# ===================================================================================================
+
+# ======================================Estate Category Elements======================================================
+Number_Of_Room_Component_Contain="name:68133.label"
+Number_Of_Room_3="css:[data-test-id='popup-listItem-439416']"
+Number_Of_Room_Component="css:[data-test-id='listing-formComponentSingleSelect-68133']"
+Parking_Selected_CheckBox="name:69191"
+Parking_CheckBox="css:[data-test-id='listing-formComponentBoolean-69191']"
+Year_Of_Construction_Component_Contain="css:[name='92367.label']"
+Year_Of_Construction_Component="css:[data-test-id='listing-formComponentSingleSelect-92367']"
+Year_Of_Construction_1396="css:[data-test-id='popup-listItem-455204']"
+Apartmant_Attribute="css:[data-test-id='popup-listItem-440477']"
+Property_Type_Component="css:[data-test-id='listing-formComponentSingleSelect-68096']"
+Property_Type_Component_Contain="css:[data-test-id='listing-formComponentSingleSelect-68096'] div input"
+Area="name:68085"
+# ===================================================================================================
+
+# ======================================Image Elements======================================================
+Image_placeholder="css:[data-test-id='upload-button-addNew']"
+Upload_file_attach="css:[data-test-id='upload-file-attach']"
+Image_loading="css:[data-test-id='image-loading']"
+# ===================================================================================================
+
+Listing_Title_TextField="name:name"
+Listing_Description_TextField="id:description"
+Listing_In_Serp="xpath://a[contains(text(),'{}')]"
